17e0bca22c0abe23ead2efd1bc3a6c6ba96f70a7
Fernando Prata <fernandoprata@gmail.com>
2016-04-04 22:32:13 +0100

    Carregamento inicial

---
+++ include/i18n/94-pt_PT-5bad1/MANIFEST.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'Build-Date' => 'Mon, 28 Mar 16 20:25:56 +0000',
+  'Build-Version' => 'v1.9.13-1-g54a70b5',
+  'Language' => 'pt_PT',
+  'Id' => 'lang:pt_PT',
+  'Last-Revision' => '2016-03-28 14:48-0400',
+  'Version' => 145919,
+);
